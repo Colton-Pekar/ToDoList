5252d3ba402652c905ba2a9dda141f56f3ca3c63
Cpeakz <coltonrpekar@gmail.com>
2021-10-12 23:32:30 -0400

    Finished, before upgrades

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,24 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ToDoList.ListComponents;
 
 namespace ToDoList.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDBContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ApplicationDBContext db)
         {
-            _logger = logger;
+            _db = db;
         }
 
-        public void OnGet()
+        [BindProperty]
+        public ListComponents.ListComponents List { get; set; }
+        public IEnumerable<ListComponents.ListComponents> ListComponent { get; set; }
+        public async Task OnGet()
         {
+            ListComponent = await _db.ListComponents.ToListAsync();
+        }
+        public async Task<IActionResult> OnPostComplete(int id)
+        {
+            var ListItem = await _db.ListComponents.FindAsync(id);
+            if (ListItem == null)
+            {
+                return NotFound();
+            }
+            ListItem.ItemComplete = !ListItem.ItemComplete;
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
 
         }
     }
